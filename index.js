0deca71340dd789320baa4fe3a31953ddc06e25f
Eric Snyder <esnyder232@gmail.com>
2020-10-05 05:37:32 -0400

    Real initial push

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 8080;
+
+//create http server
+const server = app.listen(port, () => {console.log('Webserver listening on port %s', port)});
+
+
+//adding basic http endpoints
+app.get('/', (req, res) => {res.sendFile(path.join(__dirname, "index.html"));});
+app.get('/index.html', (req, res) => {res.sendFile(path.join(__dirname, "index.html"));});
+
