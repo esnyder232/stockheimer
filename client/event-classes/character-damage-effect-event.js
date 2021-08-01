879a30abf061443db2b2044afbc016ed8f3a1834
Eric Snyder <esnyder232@gmail.com>
2021-08-01 19:24:41 -0400

    heal group added to slime healer. Also tweeked the balancing a bit

--- client/event-classes/character-damage-effect-event.js
+++ client/event-classes/character-damage-effect-event.js
@@ -1,3 +1,5 @@
+import ClientConstants from "../client-constants.js"
+
 export default class CharacterDamageEffectEvent {
 	constructor() {
 		this.gc = null;
@@ -11,33 +13,38 @@ export default class CharacterDamageEffectEvent {
 	{
 		var c = this.gc.gom.getGameObjectByServerID(e.id);
 
-		if(c !== null)
-		{
-			var dmgText = {
-				textGraphics: null,
-				countdownTimer: 750 //ms
-			};
-
-			var textStyle = {
-				color: this.gc.mainScene.damageTextColor, 
-				fontSize: "18px",
-				strokeThickness: this.gc.mainScene.damageStrokeThickness,
-				stroke: this.gc.mainScene.damageStrokeColor
-			}
-
-			dmgText.textGraphics = this.gc.mainScene.add.text((c.x * this.gc.mainScene.planckUnitsToPhaserUnitsRatio)-18, (c.y * this.gc.mainScene.planckUnitsToPhaserUnitsRatio * -1)-18, "-" + e.damage, textStyle);
-
-			this.gc.mainScene.damageTexts.push(dmgText);
-
+		if(c !== null) {
 			//show enemy damage tint if it was you that hit an enemy
-			if(e.srcUserId === this.gc.myUserServerId) {
+			if(e.srcUserId === this.gc.myUserServerId) {				
 				c.showEnemyDamageTint();
+				this.createTextGraphic(e, c);
 			}
 
 			//show self damage tint if it was you that took damage
 			if(c.ownerId === this.gc.myUserServerId) {
 				c.showSelfDamageTint();
+				this.createTextGraphic(e, c);
 			}
 		}
+	}
+
+
+	createTextGraphic(event, character) {
+		var dmgText = {
+			textGraphics: null,
+			countdownTimer: 750 //ms
+		};
+
+		var textStyle = {
+			color: this.gc.mainScene.damageTextColor, 
+			fontSize: "18px",
+			strokeThickness: this.gc.mainScene.damageStrokeThickness,
+			stroke: this.gc.mainScene.damageStrokeColor
+		}
+
+		dmgText.textGraphics = this.gc.mainScene.add.text((character.x * this.gc.mainScene.planckUnitsToPhaserUnitsRatio)-18, (character.y * this.gc.mainScene.planckUnitsToPhaserUnitsRatio * -1)-18, "-" + event.damage, textStyle);
+		dmgText.textGraphics.setDepth(ClientConstants.PhaserDrawLayers.myTextLayer);
+
+		this.gc.mainScene.damageTexts.push(dmgText);
 	}
 }
--- client/event-classes/character-heal-effect-event.js
+++ client/event-classes/character-heal-effect-event.js
@@ -1,3 +1,5 @@
+import ClientConstants from "../client-constants.js"
+
 export default class CharacterHealEffectEvent {
 	constructor() {
 		this.gc = null;
@@ -11,33 +13,37 @@ export default class CharacterHealEffectEvent {
 	{
 		var c = this.gc.gom.getGameObjectByServerID(e.id);
 
-		if(c !== null)
-		{
-			var dmgText = {
-				textGraphics: null,
-				countdownTimer: 750 //ms
-			};
-
-			var textStyle = {
-				color: this.gc.mainScene.healTextColor, 
-				fontSize: "18px",
-				strokeThickness: this.gc.mainScene.healStrokeThickness,
-				stroke: this.gc.mainScene.healStrokeColor
-			}
-
-			dmgText.textGraphics = this.gc.mainScene.add.text((c.x * this.gc.mainScene.planckUnitsToPhaserUnitsRatio)-18, (c.y * this.gc.mainScene.planckUnitsToPhaserUnitsRatio * -1)-18, "-" + e.heal, textStyle);
-
-			this.gc.mainScene.damageTexts.push(dmgText);
-
+		if(c !== null) {
 			//show teammate healing tint if it was you that healed him an enemy
 			if(e.srcUserId === this.gc.myUserServerId) {
 				c.showHealTint();
+				this.createTextGraphic(e, c);
 			}
 
 			//show self healing tint if it was you that took the heal
 			if(c.ownerId === this.gc.myUserServerId) {
 				c.showHealTint();
+				this.createTextGraphic(e, c);
 			}
 		}
+	}
+
+	createTextGraphic(event, character) {
+		var dmgText = {
+			textGraphics: null,
+			countdownTimer: 750 //ms
+		};
+
+		var textStyle = {
+			color: this.gc.mainScene.healTextColor, 
+			fontSize: "18px",
+			strokeThickness: this.gc.mainScene.healStrokeThickness,
+			stroke: this.gc.mainScene.healStrokeColor
+		}
+
+		dmgText.textGraphics = this.gc.mainScene.add.text((character.x * this.gc.mainScene.planckUnitsToPhaserUnitsRatio)-18, (character.y * this.gc.mainScene.planckUnitsToPhaserUnitsRatio * -1)-18, "+" + event.heal, textStyle);
+		dmgText.textGraphics.setDepth(ClientConstants.PhaserDrawLayers.myTextLayer);
+
+		this.gc.mainScene.damageTexts.push(dmgText);
 	}
 }
